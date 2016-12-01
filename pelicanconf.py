d3f0fe0b3b3a7076978afe4d9792deddfa1e4376
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2016-12-01 14:17:16 +0300

    Use the RFG_FAVICONS setting \o/

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,6 +23,20 @@ ARTICLE_SAVE_AS = ARTICLE_URL
 ARTICLE_LANG_URL = 'posts/{slug}-{lang}.html'
 ARTICLE_LANG_SAVE_AS = ARTICLE_LANG_URL
 
+STATIC_PATHS = ['extras', 'images']
+EXTRA_PATH_METADATA = {
+    'extras/android-chrome-192x192.png': {'path': 'android-chrome-192x192.png'},
+    'extras/android-chrome-512x512.png': {'path': 'android-chrome-512x512.png'},
+    'extras/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'extras/browserconfig.xml': {'path': 'browserconfig.xml'},
+    'extras/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+    'extras/favicon-32x32.png': {'path': 'favicon-32x32.png'},
+    'extras/favicon.ico': {'path': 'favicon.ico'},
+    'extras/manifest.json': {'path': 'manifest.json'},
+    'extras/mstile-150x150.png': {'path': 'mstile-150x150.png'},
+    'extras/safari-pinned-tab.svg': {'path': 'safari-pinned-tab.svg'},
+}
+
 PLUGIN_PATHS = ['plugins']
 PLUGINS = ['pelican-bootstrapify']
 
@@ -55,4 +69,5 @@ ICONS = [
 DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'sitemap']
 SITEMAP_SAVE_AS = 'sitemap.xml'
 
+RFG_FAVICONS = True
 PYGMENTS_STYLE = 'monokai'
